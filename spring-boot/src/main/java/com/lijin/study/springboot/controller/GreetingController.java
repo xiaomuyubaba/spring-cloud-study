e89c46f35c1147f7ac1dccfa13e709b403dcafa0
xiaomuyubaba <xmlijin.justin@163.com>
2018-10-22 22:59:31 +0800

    spring-boot

---
+++ spring-boot/src/main/java/com/lijin/study/springboot/controller/GreetingController.java
@@ -0,0 +1,23 @@
+package com.lijin.study.springboot.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lijin.study.springboot.entity.Greeting;
+
+@RestController
+public class GreetingController {
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@RequestMapping(path="/greeting", method=RequestMethod.GET)
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+
+}
